0cc9e75ed6d3703249f2f8b434aac86db17078ab
Margarida Reis <margarida.reis@ist.utl.pt>
2015-10-28 01:14:26 +0000

    added a form that automatically sends an email to me

--- website/index_login.php
+++ website/index_login.php
@@ -7,24 +7,57 @@ header("location: profile.php");
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Login Form in PHP with Session</title>
-<link href="style.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<div id="main">
-<h1>PHP Login Session Example</h1>
-<div id="login">
-<h2>Login Form</h2>
-<form action="" method="post">
-<label>UserName :</label>
-<input id="name" name="username" placeholder="username" type="text">
-<label>Password :</label>
-<input id="password" name="password" placeholder="**********" type="password">
-<input name="submit" type="submit" value=" Login ">
-<span><?php echo $error; ?></span>
-</form>
-</div>
-</div>
-</body>
+  <head>
+    <title>Watchdog : Login</title>
+    <meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="assets/css/main.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <!-- Header -->
+      <header id="header" class="alt">
+        <h1><a href="index.html">Watchdog</a></h1>
+        <nav id="nav">
+          <ul>
+            <li class="special">
+              <a href="#menu" class="menuToggle"><span>Menu</span></a>
+              <div id="menu">
+                <ul>
+                  <li><a href="index.html">Home</a></li>
+                    <!--<li><a href="generic.html">Generic</a></li>
+                  <li><a href="elements.html">Elements</a></li>
+                  <li><a href="#">Sign Up</a></li>-->
+                  <li><a href="index_login.php">Log In</a></li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+    <div id="main">
+    <div id="login">
+    <h2>Login Form</h2>
+    <form action="" method="post">
+      <input id="name" name="username" placeholder="Clinic Name" type="text">
+      <br>
+      <input id="password" name="password" placeholder="Password" type="password">
+      <br>
+      <input name="submit" type="submit" value=" Login ">
+      <span><?php echo $error; ?></span>
+    </form>
+    </div>
+    </div>
+
+    <!-- Scripts -->
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/jquery.scrollex.min.js"></script>
+      <script src="assets/js/jquery.scrolly.min.js"></script>
+      <script src="assets/js/skel.min.js"></script>
+      <script src="assets/js/util.js"></script>
+      <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+      <script src="assets/js/main.js"></script>
+
+  </body>
 </html>
--- website/profile.php
+++ website/profile.php
@@ -3,13 +3,23 @@ include('session.php');
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Your Home Page</title>
-<link href="style.css" rel="stylesheet" type="text/css">
-</head>
+  <head>
+    <title>Clinic Area</title>
+    <link href="assets/css/main.css" rel="stylesheet">
+  </head>
+
 <body>
 <div id="profile">
 <b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+
+<br>
+
+<b id="vaccine"><a href="http://192.168.43.190/arduino/digital/13/1">vaccine</a></b>
+<br>
+<b id="general appointment"><a href="http://192.168.43.190/arduino/digital/12/1">general appointment</a></b>
+
+<br>
+
 <b id="logout"><a href="logout.php">Log Out</a></b>
 </div>
 </body>
